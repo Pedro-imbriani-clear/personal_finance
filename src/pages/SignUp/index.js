fe091ac15b8d86d211d6013a03eab346e3b01734
pedro-imbriani-clear <pedro.imbriani.clear@gmail.com>
2023-08-06 16:20:48 -0300

    creating context

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Platform } from 'react-native';
 
 import { 
@@ -10,7 +10,17 @@ import {
   SubmitText
 } from '../SignIn/styles';
 
+import { AuthContext } from '../../contexts/auth';
+
+
 export default function SignUp(){
+
+  const { user } = useContext(AuthContext)
+
+  function handleSignUp(){
+    console.log(user);
+  }
+
   return(
     <Background>
       <Container
@@ -36,7 +46,7 @@ export default function SignUp(){
           />
         </AreaInput>
 
-        <SubmitButton>
+        <SubmitButton onPress={handleSignUp}>
           <SubmitText>Cadastrar</SubmitText>
         </SubmitButton>
 
