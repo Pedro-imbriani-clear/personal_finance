d8d0b9b9a5a28abf2c232d9fc2fd94a09d9b90e2
pedro-imbriani-clear <pedro.imbriani.clear@gmail.com>
2023-08-06 23:17:47 -0300

    registering user

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,5 +1,8 @@
 import React, { createContext, useState } from 'react';
 
+import api from '../services/api';
+import { useNavigation } from '@react-navigation/native';
+
 export const AuthContext = createContext({});
 
 function AuthProvider({ children }){
@@ -7,8 +10,28 @@ function AuthProvider({ children }){
     nome: 'Pedro Imbriani'
   }); 
 
+  const navigation = useNavigation();
+
+
+  async function signUp(email, password, nome){
+    try{
+      const response = await api.post('/users', {
+       name: nome,
+       password: password,
+       email: email,
+      })
+      console.log("Cadastroooou")
+
+      navigation.goBack();
+
+
+    }catch(err){
+      console.log("ERRO AO CADASTRAR", err)
+    }
+  }
+
   return(
-    <AuthContext.Provider value={{ user }}>
+    <AuthContext.Provider value={{ user, signUp }}>
       {children}
     </AuthContext.Provider>
   )
