f653ab5e02f9657c8d48678d5c6c5a80d2d0c0c1
pedro-imbriani-clear <pedro.imbriani.clear@gmail.com>
2023-08-07 23:20:02 -0300

    listing expenses

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,15 @@
 import React, { useContext, useEffect, useState } from 'react';
-import {View, Text, Button} from 'react-native';
+import { TouchableOpacity } from 'react-native';
 
 import { AuthContext } from '../../contexts/auth'
 
 import Header from '../../components/Header';
 import { 
   Background, 
-  ListBalance
+  ListBalance,
+  Area,
+  Title,
+  List
  } from './styles'; 
 
 import api from '../../services/api'
@@ -14,11 +17,15 @@ import { format } from 'date-fns';
 
 import { useIsFocused } from '@react-navigation/native';
 import BalanceItem from '../../components/BalanceItem';
+import HistoricoList from '../../components/HistoricoList';
+
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
 
 export default function Home(){
   const isFocused = useIsFocused();
   const [listBalance, setListBalance] = useState([]);
+  const [movements, setMovements] = useState([]);
 
   const [dateMovements, setDateMovements] = useState(new Date())
 
@@ -29,6 +36,12 @@ export default function Home(){
     async function getMovements(){
       let dateFormated = format(dateMovements, 'dd/MM/yyyy');
 
+      const receives = await api.get('/receives', {
+        params:{
+          date: dateFormated
+        }
+      })
+
       const balance = await api.get('/balance', {
         params:{
           date: dateFormated 
@@ -36,6 +49,7 @@ export default function Home(){
       })
 
       if(isActive){
+        setMovements(receives.data)
         setListBalance(balance.data);
       }
     }
@@ -57,6 +71,22 @@ export default function Home(){
         keyExtractor={ item => item.tag }
         renderItem={ ({ item }) => ( <BalanceItem data={item} /> )}
       />
+
+      <Area>
+        <TouchableOpacity>
+          <Icon name="event" color="#121212" size={30} />
+        </TouchableOpacity>
+        <Title>Ultimas movimentações</Title>
+      </Area>
+
+      <List
+        data={movements}
+        keyExtractor={ item => item.id }
+        renderItem={ ({ item }) => <HistoricoList data={item} />  }
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 20 }}
+      />
+
 
     </Background>
   )
